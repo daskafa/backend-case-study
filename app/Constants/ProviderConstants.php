22c472f3934c7ce9c3b7e0d472711cda988a08df
yilmaz <ydaskafa@gmail.com>
2023-07-07 23:38:35 +0300

    Added provider urls to constant file

---
+++ app/Constants/ProviderConstants.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Constants;
+
+class ProviderConstants
+{
+    const FIRST_PROVIDER_URL = 'http://www.mocky.io/v2/5d47f24c330000623fa3ebfa';
+
+    const SECOND_PROVIDER_URL = 'http://www.mocky.io/v2/5d47f235330000623fa3ebf7';
+}
