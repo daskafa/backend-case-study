cdf0551a1a00ebea5a508094e4c0882cd7a756be
yilmaz <ydaskafa@gmail.com>
2023-07-08 00:21:45 +0300

    Created GetTodosFromProviders artisan command

---
+++ app/Console/Commands/GetTodosFromProviders.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class GetTodosFromProviders extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'get-todos-from-providers';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Get todos from providers and save to database';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+
+    }
+}
